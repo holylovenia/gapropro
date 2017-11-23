917321fac1f8f0073d6e3309b2dbb5b97bc0298a
vincenthend <vincenthendrha@gmail.com>
2017-11-23 12:40:22 +0700

    Fixed Firebase chats

--- ChatService/public/javascripts/chat_manager.js
+++ ChatService/public/javascripts/chat_manager.js
@@ -8,6 +8,7 @@ var express = require('express');
 var router = express.Router();
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var request = require('request');
 router.use(cors());
 router.use(bodyParser());
 
@@ -19,38 +20,47 @@ router.post('/add_new_chat', function (req, res, next) {
     var sId = parseInt(req.body.senderId);
     var rId = parseInt(req.body.receiverId);
     var m = req.body.chatMessage;
+    var token = "";
+    Firebase.findOne({'user_id': rId}, function (err, firebase) {
+        if (err) throw err;
+        token = firebase.fb_token;
 
-    var token = Firebase.findOne({'user_id': rId}, function (err, firebase) {
-       if (err) throw err;
-    });
+        console.log(token);
+        var options = {
+            method: 'post',
+            json: true,
+            url: "https://fcm.googleapis.com/fcm/send",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "key=AIzaSyDLWhhAQ3MmOPvhwXCny7kLw_HyWWq2A6g"
+            },
+            body: {
+                "data": {
+                    "notification": {
+                        "body": "Halo",
+                        "title": "hai"
+                    }
+                },
 
-    var options = {
-        method: 'post',
-        json: true,
-        url: "https://fcm.googleapis.com/fcm/send",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization:": "key=AAAA5p-6zdA:APA91bFLGzZ2w0jSjpU_g3BQ18vV6XQenkolJUYa1EnjtABldwvl82vMq_SBYJw8Jw6zL8PeubIDaDCjhFqmSTNkDqkLboWuVQEQodLYq3v5B8JLubWiu8d2mOEeRZRK5NwhocUeaMvA"
-        },
-        body: {
-            "data": "INCOMING",
-            "to": token
-        }
-    };
+                "to": token
+            }
+        };
 
-    request(options, function (err, res, body) {
-        if (err) {
-            console.log('Error :', err);
-            return
-        }
-        console.log(' Body :', body);
-        var chat = new Chat({
-            sender_id: sId,
-            receiver_id: rId,
-            message: m
+        request(options, function (err, res, body) {
+            if (err) {
+                console.log('Error :', err);
+                return
+            }
         });
-        chat.save();
     });
+
+
+    var chat = new Chat({
+        sender_id: sId,
+        receiver_id: rId,
+        message: m
+    });
+    chat.save();
 
     res.set('Content-Type', 'application/json');
     res.send('{"status":true}');
